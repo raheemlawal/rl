cad823ec60162bb1ba072261769c8d549ce670ef
Raheem Lawal <lawalraheem@gmail.com>
2026-01-27 16:00:34 -0800

    add new photos and format

--- src/App.js
+++ src/App.js
@@ -1,73 +1,122 @@
 import './App.css';
+import { useState } from 'react';
 import a from './photos/miami-jm.JPG';
 import b from './photos/desert-pink.jpeg';
 import c from './photos/miami-rarity.JPG';
 import d from './photos/vrc-jump.jpg';
 import e from './photos/on-boardwalk.jpg'
 import f from './photos/lonelyheartsthrow.JPG'
-import g from './photos/boat.JPG'
-import h from './photos/miami-kitboys.JPG'
 import i from './photos/hokafromback.JPG'
-import j from './photos/hokajump.JPG'
 import k from './photos/hokarunnoface.JPG'
 import l from './photos/hokaside.JPG'
+import m from './photos/Cburkesmith-VeniceBeach-11-2025-16.jpg'
+import n from './photos/Cburkesmith-VeniceBeach-11-2025-31.jpg'
+import o from './photos/Cburkesmith-VeniceBeach-11-2025-62.jpg'
+import p from './photos/Cburkesmith-VeniceBeach-11-2025-67.jpg'
+import q from './photos/Cburkesmith-VeniceBeach-11-2025-75.jpg'
+import r from './photos/KRE_8182.jpg'
+import s from './photos/KRE_8203.jpg'
+import t from './photos/KRE_8332.jpg'
 
-import {Container, Row, Image, Col } from 'react-bootstrap';
+import {Container, Row, Image, Col, Modal } from 'react-bootstrap';
 
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <Container className='App'>
       <Row id='top-row'>
         <h1 className = 'my-name'>RAHEEM LAWAL</h1>
-        <h6 className = 'my-name'>MODEL</h6>
         <h6 className = 'my-name'>LOS ANGELES</h6>
-        <h6 className = 'my-name'>INQUIRIES: LAWALRAHEEM@GMAIL.COM</h6>
+        <h6 className = 'my-name'>INQUIRIES: HEY@ITSRAHEEM.COM</h6>
+        <h6 className = 'my-name'>FEATURES: ON/CLASSPASS/HOKA</h6>
       </Row>
-      <Col className='pics'>
-        <Row id="pics-row-one">
-            <Col id = "a1">
-              <Image src = {a} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+      <Row className='pics'>
+        <Col className="column-one">
+          <Image src = {s} className = "img-responsive column-one-img" alt = "Featured" onClick={() => handleImageClick(b)} style={{cursor: 'pointer'}}/>
+          <Image src = {l} className = "img-responsive column-one-img" alt = "Featured" onClick={() => handleImageClick(l)} style={{cursor: 'pointer'}}/>
+        </Col>
+        <Col className="column-two">
+          <Row className="column-two-grid">
+            <Col className="grid-item">
+              <Image src = {o} className = "img-responsive grid-img" alt = "Portfolio" onClick={() => handleImageClick(i)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "b1">
-              <Image src = {b} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+            <Col className="grid-item">
+              <Image src = {m} className = "img-responsive grid-img" alt = "Portfolio" onClick={() => handleImageClick(m)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "c1">
-              <Image src = {d} className = "img-responsive" alt = "Cinque Terre" width={375} height={375}/>
+          </Row>
+          <Row className="column-two-grid">
+            <Col className="grid-item">
+              <Image src = {b} className = "img-responsive grid-img" alt = "Portfolio" onClick={() => handleImageClick(e)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "d1">
-              <Image src = {l} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+            <Col className="grid-item">
+              <Image src = {a} className = "img-responsive grid-img" alt = "Portfolio" onClick={() => handleImageClick(a)} style={{cursor: 'pointer'}}/>
             </Col>
-        </Row>
-        <Row id="pics-row-one">
-            <Col id = "e1">
-              <Image src = {c} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+          </Row>
+        </Col>
+        <Col className="column-three">
+          <Row className="column-three-grid">
+            <Col className="small-grid-item">
+              <Image src = {d} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(d)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "f1">
-              <Image src = {e} className = "img-responsive" alt = "Cinque Terre" width={375} height={375}/>
+            <Col className="small-grid-item">
+              <Image src = {c} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(c)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "g1">
-              <Image src = {g} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+          </Row>
+          <Row className="column-three-grid">
+            <Col className="small-grid-item">
+              <Image src = {f} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(f)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "h1">
-              <Image src = {f} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+            <Col className="small-grid-item">
+              <Image src = {k} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(k)} style={{cursor: 'pointer'}}/>
             </Col>
-        </Row>
-        <Row id="pics-row-one">
-            <Col id = "i1">
-              <Image src = {i} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+          </Row>
+          <Row className="column-three-grid">
+            <Col className="small-grid-item">
+              <Image src = {n} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(n)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "j1">
-              <Image src = {j} className = "img-responsive" alt = "Cinque Terre" width={375} height={375}/>
+            <Col className="small-grid-item">
+              <Image src = {i} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(o)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "k1">
-              <Image src = {k} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+            <Col className="small-grid-item">
+              <Image src = {p} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(p)} style={{cursor: 'pointer'}}/>
             </Col>
-            <Col id = "l1">
-              <Image src = {h} className = "img-responsive" alt = "Cinque Terre" width={300} height={375}/>
+          </Row>
+          <Row className="column-three-grid">
+            <Col className="small-grid-item">
+              <Image src = {q} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(q)} style={{cursor: 'pointer'}}/>
             </Col>
-        </Row>  
-      </Col>
+            <Col className="small-grid-item">
+              <Image src = {r} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(r)} style={{cursor: 'pointer'}}/>
+            </Col>
+            <Col className="small-grid-item">
+              <Image src = {e} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(s)} style={{cursor: 'pointer'}}/>
+            </Col>
+          </Row>
+          <Row className="column-three-grid">
+            <Col className="small-grid-item">
+              <Image src = {t} className = "img-responsive small-grid-img" alt = "Portfolio" onClick={() => handleImageClick(t)} style={{cursor: 'pointer'}}/>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+
+      <Modal show={selectedImage !== null} onHide={handleCloseModal} centered size="lg">
+        <Modal.Body style={{padding: 0, backgroundColor: 'black'}}>
+          {selectedImage && (
+            <Image src={selectedImage} fluid style={{width: '100%', height: 'auto'}} />
+          )}
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 }
